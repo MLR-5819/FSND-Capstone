6bb52920fab5bad6abf74de97179258fd3d34e3a
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-09-10 21:25:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 export FLASK_app=app.py
 export FLASK_ENV=development
 
-export AUTH0_DOMAIN=  info
+export DATABASE_URL=postgres://postgres@localhost:5432/thisorthat
 
-export DATABASE_URL = 'postgres://postgres@localhost:5432/thisorthat'
+export AUTH0_DOMAIN=dev-mrose.us.auth0.com
+export ALGORITHMS=RS256
+export API_AUDIENCE= #createauth0api
