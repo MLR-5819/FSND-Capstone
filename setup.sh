e62e937b3a28124699dbbb07eb62aced7a5b2d3e
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-09-19 23:20:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ export DATABASE_URL=postgres://postgres@localhost:5432/thisorthat
 
 export AUTH0_DOMAIN=dev-mrose.us.auth0.com
 export ALGORITHMS=RS256
-export API_AUDIENCE= #createauth0api
+export API_AUDIENCE=tot
