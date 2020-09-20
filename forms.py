fb94164a30ce7a2808c69fa3fa928182ccc126a8
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-09-19 20:57:30 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -8,3 +8,9 @@ class AddEntryForm(FlaskForm):
     category = SelectField('Choose a Category:', coerce=int)
     url = TextField('Photo URL Link:', [DataRequired()])
     submit = SubmitField('Submit')
+
+class UpdateEntryForm(FlaskForm):
+    # Update entry form
+    name = TextField('Updated Name:', [DataRequired(), Length(max=16, message=('Your title is too long.'))])
+    category = SelectField('Updated Category:', coerce=int)
+    submit = SubmitField('Submit')
