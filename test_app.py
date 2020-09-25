51881947f6be14f9e4964f83b6c4df358a84a5bb
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-09-24 23:00:17 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,1 +1,36 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
 
+from app import create_app
+from models import setup_db, Entry, Category
+
+class ThisOrThatTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "thisorthat"
+        self.database_path = "postgres://{}:{}@{}/{}".format('postgres','password','localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+    
+    def tearDown(self):
+        #Executed after reach test
+        pass
+
+    def test_index(self):
+        result = self.client().get('/')
+        self.assertEqual(result.status_code, 200)
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
