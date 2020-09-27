32ee902d4a21198cb16d06a32c164f57ae88a010
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-09-26 22:25:19 -0500

    updates

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 export FLASK_app=app.py
 export FLASK_ENV=development
 
-export DATABASE_URL=postgres://postgres@localhost:5432/thisorthat
+export DATABASE_URL=postgres://tnutsstcnxcrvi:0c761b37fad3aaf48cd854e1f5681d272c45b647f983159cdf9995acbbc92d1d@ec2-34-192-122-0.compute-1.amazonaws.com:5432/dalv5bn6pr7eqi
 
 export AUTH0_DOMAIN=dev-mrose.us.auth0.com
 export ALGORITHMS=RS256
