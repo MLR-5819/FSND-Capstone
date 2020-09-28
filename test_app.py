711c2af71758ad5f51e90e90f4a9d899eb5e0199
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-09-27 20:51:39 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,6 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 from app import create_app
 from models import setup_db, Entry, Category
 
+user_jwt = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImtoX3drdVlWNGwzT1FIX2pvaGZ6MSJ9.eyJpc3MiOiJodHRwczovL2Rldi1tcm9zZS51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWY3MGQxZjBhNTExZmUwMDZiNzhmZDQwIiwiYXVkIjoidG90IiwiaWF0IjoxNjAxMjU0NTc0LCJleHAiOjE2MDEyNjE3NzQsImF6cCI6ImtZV0JvdjV0eWlZazkwRTZvc0dXaHRGNUlLeENmS3pOIiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJwYXRjaDplbnRyeSIsInBvc3Q6ZW50cnkiXX0.VKeXZrDQ8rZsmDkJUhaEqR7F1ETBrvbbp_9EUvtR7q3OQJptgSrLzwzSGVeDrgSAYOPI0Esw3qiIkw-0UaVNYBwV2J1l2L2I5AMp2imfMc0E8KPJa3YxCWKV8Ji8-GOGdngyQm-Cv4LwKR4GZiCTbQocISNLuwzN8EjhtylKJkE_6hwu2eMecMxB3rMxXrPMUpyq_IpEDdxu2UKOrKfRrLWvdkgxdR5Z9T0gO55DSTWCYnTbemdQnQfCgIxBZBbhZNGBeF_5o8T21cdyR2c1yXb0bx22YoHxSr5B_wNuIvsk2UgPCCQvYjM6Q5tC7_adubTBZGHc7K8wCJUJc3A0LQ'
+admin_jwt = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImtoX3drdVlWNGwzT1FIX2pvaGZ6MSJ9.eyJpc3MiOiJodHRwczovL2Rldi1tcm9zZS51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWY3MGQyYWI2MzAwMjQwMDcxOTM4ZmY1IiwiYXVkIjoidG90IiwiaWF0IjoxNjAxMjU0NDk5LCJleHAiOjE2MDEyNjE2OTksImF6cCI6ImtZV0JvdjV0eWlZazkwRTZvc0dXaHRGNUlLeENmS3pOIiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJkZWxldGU6ZW50cnkiLCJwYXRjaDplbnRyeSIsInBvc3Q6ZW50cnkiXX0.QMXDam7eiZlmaWpKyT6HZVgq9EXVR3mLmxF_xutmvikG6b8DJg_3_yHqamrLsBYsZel2PQlwG8WgXkXh3z5NkzrPyp2EvJ1dYWqK3ZSVzpKo5dv7A-16oxci30UR_1gpOCDTLUYhkCVtuxIvrWi33G7xIIMmB-0aJSujWynni_ojGO_4WPvl2XDsTG6We9yy8RP8KcOyqhUdJ6-Bgt5p-NFmuEq1epk063aPRZzgRf8tzwXu-Xu1AjOXqeKtVCRaCoDwSZ6KTUXyy41ETvoosiSWKcl8CdIRl_dgcHEAVA99gWrNdzc3YfSC7DRtVcKBdrCX7OPwp0Ya7PkPLbt3BQ'
+        
 class ThisOrThatTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -82,18 +85,18 @@ class ThisOrThatTestCase(unittest.TestCase):
         self.assertTrue(data['categories'])
         self.assertTrue(data['entries'])
 
-    #def test_post_add_entry(self):
-    #    new_entry = {
-    #        'name': 'test entry',
-    #        'category': 1,
-    #        'url': 'www.google.com'
-    #    }
+    def test_post_add_entry(self):
+        new_entry = {
+            'name': 'test entry',
+            'category': 1,
+            'url': 'www.google.com'
+        }
 
-    #    result = self.client().post('/api/entries/add', json=new_entry)
-    #    data = json.loads(result.data)
+        result = self.client().post('/api/entries/add', json=new_entry)
+        data = json.loads(result.data)
 
-    #    self.assertEqual(result.status_code, 200)
-    #    self.assertEqual(data['success'], True)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(data['success'], True)
 
     def test_400_failed_add_entry(self):
         result = self.client().post('/api/entries/add', data=dict(name='test no url', category='1', url=''))
@@ -105,7 +108,7 @@ class ThisOrThatTestCase(unittest.TestCase):
         
     
     def test_get_update_entry(self):
-        result = self.client().get('/api/entries/1/update')
+        result = self.client().get('/api/entries/1/update', headers={'Authorization': 'Bearer ' + user_jwt})
         data = json.loads(result.data)
 
         self.assertEqual(result.status_code, 200)
@@ -113,18 +116,18 @@ class ThisOrThatTestCase(unittest.TestCase):
         self.assertTrue(data['category'])
         self.assertTrue(data['entry'])
 
-    #def test_401_patch_not_auth_update_entry(self):
-    #    patch_entry = {
-    #        'id': 46,
-    #        'name': 'updated test entry',
-    #        'category': 2
-    #    }
+    def test_401_patch_not_auth_update_entry(self):
+        patch_entry = {
+            'id': 46,
+            'name': 'updated test entry',
+            'category': 2
+        }
         
-    #    result = self.client().patch('/api/entries/46/update', json=patch_entry)
-    #    data = json.loads(result.data)
+        result = self.client().patch('/api/entries/46/update', json=patch_entry)
+        data = json.loads(result.data)
 
-    #    self.assertEqual(result.status_code, 401)
-    #    self.assertEqual(data['success'], False)
+        self.assertEqual(result.status_code, 401)
+        self.assertEqual(data['success'], False)
 
     def test_patch_update_entry(self):
         #update API endpoint ID to match your database
@@ -134,8 +137,8 @@ class ThisOrThatTestCase(unittest.TestCase):
             'category': 2
         }
 
-        #add in auth
-        result = self.client().patch('/api/entries/46/update', json=patch_entry)
+        #User level authorization
+        result = self.client().patch('/api/entries/46/update', json=patch_entry, headers={'Authorization': 'Bearer ' + user_jwt})
         data = json.loads(result.data)
 
         self.assertEqual(result.status_code, 200)
@@ -148,8 +151,8 @@ class ThisOrThatTestCase(unittest.TestCase):
             'name': 'updated test entry',
             'category': 2
         }
-        #add in auth
-        result = self.client().patch('/api/entries/100000/update', json=patch_entry)
+        #User level auth
+        result = self.client().patch('/api/entries/100000/update', json=patch_entry, headers={'Authorization': 'Bearer ' + user_jwt})
         data = json.loads(result.data)
 
         self.assertEqual(result.status_code, 404)
@@ -157,9 +160,8 @@ class ThisOrThatTestCase(unittest.TestCase):
         self.assertEqual(data['message'], 'Resource Not Found')
 
     def test_400_failed_update_entry(self):
-        #addinauth
         #update API endpoint ID to match your database
-        result = self.client().patch('/api/entries/49/update', data=dict(name='test no category', category=''))
+        result = self.client().patch('/api/entries/49/update', data=dict(name='test no category', category=''), headers={'Authorization': 'Bearer ' + user_jwt})
         data = result.get_json()
 
         self.assertEqual(result.status_code, 400)
@@ -168,32 +170,29 @@ class ThisOrThatTestCase(unittest.TestCase):
 
     def test_delete_entry(self):
         #update API endpoint ID to match your database
-        result = self.client().get('/api/entries/48/delete')
+        #Admin level auth
+        result = self.client().delete('/api/entries/51/delete', headers={'Authorization': 'Bearer ' + admin_jwt})
         data = json.loads(result.data)
-        #addinauth
+        
         self.assertEqual(result.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_401_not_auth_delete_entry(self):
         #update API endpoint ID to match your database
-        result = self.client().get('/api/entries/48/delete')
+        #user level auth, not authorized to delete
+        result = self.client().delete('/api/entries/51/delete', headers={'Authorization': 'Bearer ' + user_jwt})
         data = json.loads(result.data)
 
         self.assertEqual(result.status_code, 401)
         self.assertEqual(data['success'], False)
     
     def test_404_failed_delete_no_entry(self):
-        result = self.client().get('/api/entries/100000/delete')
+        result = self.client().delete('/api/entries/100000/delete', headers={'Authorization': 'Bearer ' + admin_jwt})
         data = json.loads(result.data)
 
         self.assertEqual(result.status_code, 404)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Resource Not Found')
-
-    #def test_422_database_error(self):
-
-    #405 error
-    #500 error
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
