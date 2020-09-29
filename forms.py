218718de7cebcf144ed6b146247fef87d6258e2c
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-09-28 21:27:44 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,15 +2,23 @@ from flask_wtf import FlaskForm
 from wtforms import TextField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length
 
+
 class AddEntryForm(FlaskForm):
     # Add new entry form
-    name = TextField('Entry Name:', [DataRequired(), Length(max=16, message=('Your title is too long.'))])
+    name = TextField(
+        'Entry Name:',
+        [DataRequired(),
+         Length(max=16, message=('Your title is too long.'))])
     category = SelectField('Choose a Category:', coerce=int)
     url = TextField('Photo URL Link:', [DataRequired()])
     submit = SubmitField('Submit')
 
+
 class UpdateEntryForm(FlaskForm):
     # Update entry form
-    name = TextField('Updated Name:', [DataRequired(), Length(max=16, message=('Your title is too long.'))])
+    name = TextField(
+        'Updated Name:',
+        [DataRequired(),
+         Length(max=16, message=('Your title is too long.'))])
     category = SelectField('Updated Category:', coerce=int)
     submit = SubmitField('Submit')
